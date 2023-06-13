41f467c72f93d5e9f90d6ff96b57ef75ec513b84
countrostopchin <121770619+countrostopchin@users.noreply.github.com>
2023-06-13 14:20:43 -0400

    Create gf2_n.py

---
+++ gf2_n.py
@@ -0,0 +1,9 @@
+import gf2
+
+gf2_n_poly_prod(p1, p2, f):
+  """Assumes that p1 and p2 are polynomials
+  whose coefficients are bitvectors of length n,
+  viewed as elements of GF(2^n), and that f is the
+  corresponding modulus for GF(2^n), returns the
+  product of p1 and p2."""
+  pass
